5416bcdfd2109edf45d47f1957aafdc0541bf541
Daniel Weber <1678401@park.edu>
2021-11-10 21:21:56 -0600

    changed favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,8 +11,17 @@ module.exports = {
   },
   plugins: [
     "gatsby-plugin-image",
-    "gatsby-plugin-sharp",
+
     "gatsby-transformer-sharp",
+
+    "gatsby-plugin-sharp",
+
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/favicon.png",
+      },
+    },
     {
       resolve: "gatsby-source-contentful",
       options: {
